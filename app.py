fb1e87605c69a931295fdabea49ca737265c309b
Matt Reidy <matt@ereidy.com>
2023-05-25 12:53:03 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Understand your Report")
+    st.header("Undersand your NGS Report")
+    # upload file
+    pdf = st.file_uploader("Upload your Report PDF", type="pdf")
+    # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=2000,
+        chunk_overlap=400,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      # show user input
+      user_question = st.text_input("Ask a question about your Report:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        llm = OpenAI(model_name='gpt-3.5-turbo')
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+        st.write(response)
+
+if __name__ == '__main__':
+    main()
